7e7c371f03fa39f3cbe8619393e8fa988abbea45
Fabio <fabiomtv@outlook.com.br>
2022-12-30 01:20:04 -0300

    att

---
+++ ExercicioFixacaoV2/ExercicioFixacaoV2/Agencia.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacaoV2
+{
+    internal class Agencia
+    {
+        public int Titular { get; set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public Agencia(int titular, string nome)
+        {
+            Titular = titular;
+            Nome = nome;
+        }
+
+        public Agencia(int titular, string nome, double saldo) : this(titular, nome)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo -= saque;
+            Saldo -= 5;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome
+                + ", Titular: " + Titular
+                + ", Saldo: " + Saldo
+                + " ";
+        }
+    }
+}
---
+++ ExercicioFixacaoV2/ExercicioFixacaoV2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacaoV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Agencia CaixaEletronico;
+
+            Console.Write("Digite o número da Conta: ");
+            int titular = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o nome do Titular: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Deseja efetuar um depósito inicial? (s/n): ");
+            char deposito = char.Parse(Console.ReadLine());
+            if (deposito == 's' || deposito == 'S')
+            {
+                Console.WriteLine();
+                Console.Write("Digite o valor do depósito: ");
+                double depositoinicial = double.Parse(Console.ReadLine());
+                CaixaEletronico = new Agencia(titular, nome, depositoinicial);
+            }
+            else
+            {
+                CaixaEletronico = new Agencia(titular, nome);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(CaixaEletronico);
+
+            Console.WriteLine();
+            Console.Write("Deseja realizar mais um Depósito: ");
+            double valor = double.Parse(Console.ReadLine());
+            CaixaEletronico.Deposito(valor);
+
+            Console.WriteLine();
+            Console.WriteLine(CaixaEletronico);
+
+            Console.WriteLine();
+            Console.Write("Deseja realizar um Saque: ");
+            valor = double.Parse(Console.ReadLine());
+            CaixaEletronico.Saque(valor);
+
+            Console.WriteLine();
+            Console.WriteLine(CaixaEletronico);
+        }
+    }
+}
