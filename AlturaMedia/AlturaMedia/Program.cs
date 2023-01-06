fc3a80d76d1f32a74c156302f61d23d50bd9ea23
Fabio <fabiomtv@outlook.com.br>
2023-01-06 16:49:43 -0300

    att

--- AlturaMedia/AlturaMedia/Program.cs
+++ AlturaMedia/AlturaMedia/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,29 @@ namespace AlturaMedia
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite o número de pessoas: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] vetor = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite uma altura válida: ");
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                soma+= vetor[i];
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine("Média das alturas foi de: " + media.ToString("F2"), CultureInfo.InvariantCulture);
+
+
+
         }
     }
 }
