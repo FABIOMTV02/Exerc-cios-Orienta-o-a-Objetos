bfca76d9bdce7e4073f4db33e25e0c695526bec9
Fábio Martins <fabiomtv@outlook.com.br>
2023-01-06 19:46:30 -0300

    att

---
+++ ExercicioFixacaoPensao/ExercicioFixacaoPensao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExercicioFixacaoPensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quarto[] vetor = new Quarto[10];
+
+            Console.Write("Digite a quantidade de quartos alugados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n ; i++)
+
+            {
+                Console.WriteLine();
+                Console.WriteLine(i + "° Aluguel:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Apartamento: ");
+                int apartamento = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                vetor[apartamento] = new Quarto (nome, email);
+
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                
+                if (vetor[i] != null)
+                {
+                    Console.WriteLine("Quarto " + i + ": " + vetor[i]);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ ExercicioFixacaoPensao/ExercicioFixacaoPensao/Quarto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioFixacaoPensao
+{
+    class Quarto
+    {
+        public string Nome{ get; set; }
+        public string Email { get; set; }
+        public int Apartamento { get; set; }
+
+
+        public Quarto(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
